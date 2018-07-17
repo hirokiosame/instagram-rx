0acf1626de24a6a4169e788c6dda53461b2b3a10
Hiroki Osame <hiroki@weebly.com>
2018-07-17 01:05:26 +0100

    Updated user tests

--- index.user.spec.js
+++ index.user.spec.js
@@ -6,20 +6,41 @@ const util = require('util');
 test('Should get user details', async () => {
 	const ig = new Instagram();
 
-	const details = await ig.user('nikitz___').details().toPromise();
+	const details = await ig.user('private.number_').details().toPromise();
 	expect(details).toHaveProperty('id');
 	expect(details).toHaveProperty('username');
 }, 1000 * 1000);
 
 test('Should get user posts', (done) => {
 	const ig = new Instagram();
-	const wiretap = jest.fn(/*console.log*/);
+	const wiretap = jest.fn(p => console.log(util.inspect(p, { colors: true })));
 
-	ig.user('nikitz___').posts()
+	ig.user('private.number_').posts()
+		.pipe(
+			operators.take(10)
+		)
 		.subscribe({
 			next: wiretap,
 			complete() {
 				expect(wiretap).toHaveBeenCalled();
+				done();
+			},
+			error(err) {
+				console.error(err);
+			},
+		});
+}, 1000 * 1000);
+
+
+test('Should not get private user posts', (done) => {
+	const ig = new Instagram();
+	const wiretap = jest.fn(console.log);
+
+	ig.user('bevwins').posts()
+		.subscribe({
+			next: wiretap,
+			complete() {
+				expect(wiretap).not.toHaveBeenCalled();
 				done();
 			},
 			error(err) {
